5b3c21f07059ea264240f4207f61de231d92810a
WoutervanDixhoorn <9KsmQ3N6>
2022-05-03 01:19:42 +0200

    Readded bootstrap-vue
    Keuze stress

--- recepten_frontend/src/views/Home.vue
+++ recepten_frontend/src/views/Home.vue
@@ -4,6 +4,26 @@ import { getAPI }  from '../axios-api'
 import Navbar from '../components/Navbar.vue'
 
 export default {
+    data() {
+        return {
+        name: 'BootstrapVue',
+        show: true
+        }
+    },
+    watch: {
+        show(newVal) {
+        console.log('Alert is now ' + (newVal ? 'visible' : 'hidden'))
+        }
+    },
+    methods: {
+        toggle() {
+        console.log('Toggle button clicked')
+        this.show = !this.show
+        },
+        dismissed() {
+        console.log('Alert dismissed')
+        }
+    },
     components: {
         Navbar,
     }   
@@ -13,6 +33,17 @@ export default {
 <template>
     <div>
         <Navbar />
+        <b-button size="sm" @click="toggle">
+            {{ show ? 'Hide' : 'Show' }} Alert
+        </b-button>
+        <b-alert
+            v-model="show"
+            class="mt-3"
+            dismissible
+            @dismissed="dismissed"
+        >
+            Hello {{ name }}!
+        </b-alert>
     </div>
 </template>
 
