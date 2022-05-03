8cb0374c05f8504b89a8263a863c613adf6c8a64
WoutervanDixhoorn <9KsmQ3N6>
2022-05-03 15:16:19 +0200

    Added persistent store | So keep logged in after refresh :tada:

--- recepten_frontend/src/store/index.js
+++ recepten_frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import AccountModule from './modules/account'
 
 Vue.use(Vuex)
@@ -10,5 +11,6 @@ export default new Vuex.Store({
   },
   modules: {
     account: AccountModule,
-  }
+  },
+  plugins: [createPersistedState()]
 }); 
